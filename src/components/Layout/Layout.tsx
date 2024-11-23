d104db06c90054acd31b10e003f894005ceed1a6
AliArif5781 <aliairf92@gmail.com>
2024-11-23 11:50:52 +0500

    Add Layout.tsx

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,13 @@
+import Navbar from "../Common/Navbar";
+import { Outlet } from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <div>
+      <Navbar />
+      <Outlet />
+    </div>
+  );
+};
+
+export default Layout;
